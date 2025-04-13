6ea5d7f43c7a07898231606ae1385f53bc8b8edd
NgoVanPhuoc2006 <136105792+NgoVanPhuoc2006@users.noreply.github.com>
2025-04-13 15:53:04 +0700

    Update app.py

--- app.py
+++ app.py
@@ -32,5 +32,6 @@ def home():
         result = classify_sentiment(text)
     return render_template("index.html", result=result)
 
+# Dùng cho chạy local, không ảnh hưởng khi deploy trên Render
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=5000)
